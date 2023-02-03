23179617aa9ce1a99904a9f608687b9753c29ecd
alex <rwrx@foxmail.com>
2023-02-03 15:42:13 +0800

    feat: unique id generator

---
+++ uniqid/uniqid.go
@@ -0,0 +1,16 @@
+package uniqid
+
+import (
+	"fmt"
+	"time"
+
+	"github.com/o8x/jk/djb2"
+)
+
+func Number() uint64 {
+	return djb2.Sum(fmt.Sprintf("%d", time.Now().UnixNano()))
+}
+
+func String() string {
+	return djb2.Make(fmt.Sprintf("%d", time.Now().UnixNano()))
+}
