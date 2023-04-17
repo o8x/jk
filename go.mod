36c87709bc9fd37644e558d7764b16f0ed3d4dee
alex <rwrx@foxmail.com>
2023-04-17 14:44:06 +0800

    feat: system service

--- go.mod
+++ go.mod
@@ -4,9 +4,10 @@ go 1.19
 
 require (
 	github.com/glebarez/go-sqlite v1.20.3
+	github.com/kardianos/service v1.2.2
 	github.com/mattn/go-sqlite3 v1.14.16
 	github.com/sirupsen/logrus v1.9.0
-	golang.org/x/net v0.6.0
+	golang.org/x/net v0.7.0
 	gopkg.in/natefinch/lumberjack.v2 v2.2.1
 )
 
@@ -15,6 +16,7 @@ require (
 	github.com/google/uuid v1.3.0 // indirect
 	github.com/mattn/go-isatty v0.0.17 // indirect
 	github.com/remyoudompheng/bigfft v0.0.0-20230129092748-24d4a6f8daec // indirect
+	github.com/stretchr/testify v1.8.0 // indirect
 	golang.org/x/sys v0.5.0 // indirect
 	golang.org/x/text v0.7.0 // indirect
 	modernc.org/libc v1.22.3 // indirect
---
+++ sc/service.go
@@ -0,0 +1,89 @@
+package sc
+
+import (
+	"errors"
+
+	"github.com/kardianos/service"
+)
+
+var (
+	s service.Service
+)
+
+type Action struct {
+	start func()
+	stop  func()
+}
+
+type Preferences = service.Config
+
+func (a Action) Start(service.Service) error {
+	if a.start == nil {
+		return nil
+	}
+
+	go a.start()
+	return nil
+}
+
+func (a Action) Stop(service.Service) error {
+	if a.stop == nil {
+		return nil
+	}
+
+	go a.stop()
+	return nil
+}
+
+func Init(start, stop func(), conf *Preferences) error {
+	sv, err := service.New(Action{start: start, stop: stop}, conf)
+	if err != nil {
+		return err
+	}
+
+	s = sv
+	return nil
+}
+
+func Get() *service.Service {
+	return &s
+}
+
+func Install() bool {
+	return s.Install() != nil
+}
+
+func Start() error {
+	return s.Run()
+}
+
+func Reinstall() error {
+	if !NotInstalled() {
+		if err := s.Uninstall(); err != nil {
+			return err
+		}
+	}
+
+	return s.Install()
+}
+
+func UnInstall() bool {
+	err := s.Uninstall()
+	return err != nil
+}
+
+func NotInstalled() bool {
+	_, err := s.Status()
+	return errors.Is(err, service.ErrNotInstalled)
+}
+
+func Status() service.Status {
+	status, err := s.Status()
+	if err != nil {
+		if errors.Is(err, service.ErrNotInstalled) {
+			return service.StatusUnknown
+		}
+	}
+
+	return status
+}
